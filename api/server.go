9dcdc3a17fdfdf86a2fa3e53f940da3f237d0c86
bagashiz <bagashiz@proton.me>
2022-09-26 15:51:41 +0700

    setup Restful HTTP API using Gin

---
+++ api/server.go
@@ -0,0 +1,32 @@
+package api
+
+import (
+	db "github.com/bagashiz/Simple-Bank/db/sqlc"
+	"github.com/gin-gonic/gin"
+)
+
+// Server serves HTTP requests for banking service.
+type Server struct {
+	store  *db.Store
+	router *gin.Engine
+}
+
+// NewServer creates a new HTTP server and setup routing.
+func NewServer(store *db.Store) *Server {
+	server := &Server{store: store}
+	router := gin.Default()
+
+	server.router = router
+
+	return server
+}
+
+// Start runs the HTTP server on a specific address.
+func (server *Server) Start(address string) error {
+	return server.router.Run(address)
+}
+
+// errorResponse is a common format for API errors.
+func errorReponse(err error) gin.H {
+	return gin.H{"error": err.Error()}
+}
