383a69496a99f77485ff52959ab61304ba8c196c
bagashiz <bagashiz@proton.me>
2022-11-07 19:48:13 +0700

    add login router & refactor code

--- api/server.go
+++ api/server.go
@@ -30,22 +30,29 @@ func NewServer(config util.Config, store db.Store) (*Server, error) {
     store:      store,
     tokenMaker: tokenMaker,
   }
-	router := gin.Default()
 
 	if v, ok := binding.Validator.Engine().(*validator.Validate); ok {
 		v.RegisterValidation("currency", validCurrency)
 	}
 
-	router.POST("/accounts", server.createAccount)
-	router.POST("/transfers", server.createTransfer)
-	router.POST("/users", server.createUser)
+  server.setupRouter()
 
+	return server, nil
+}
+
+func (server *Server) setupRouter() {
+	router := gin.Default()
+  
+	router.POST("/users", server.createUser)
+	router.POST("/users/login", server.loginUser)
+  
+	router.POST("/accounts", server.createAccount)
 	router.GET("/accounts/:id", server.getAccount)
 	router.GET("/accounts/", server.listAccounts)
 
+	router.POST("/transfers", server.createTransfer)
+  
 	server.router = router
-
-	return server, nil
 }
 
 // Start runs the HTTP server on a specific address.
