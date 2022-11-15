34089fcc0a0f920d3f3d92a2388b08775ea87f4f
bagashiz <work.bitsy@simplelogin.com>
2022-11-15 13:50:36 +0700

    testing create account API

--- api/account_test.go
+++ api/account_test.go
@@ -15,6 +15,7 @@ import (
 	db "github.com/bagashiz/Simple-Bank/db/sqlc"
 	"github.com/bagashiz/Simple-Bank/token"
 	"github.com/bagashiz/Simple-Bank/util"
+	"github.com/gin-gonic/gin"
 	"github.com/golang/mock/gomock"
 	"github.com/stretchr/testify/require"
 )
@@ -153,6 +154,132 @@ func TestGetAccountAPI(t *testing.T) {
 			request, err := http.NewRequest(http.MethodGet, url, nil)
 			require.NoError(t, err)
 
+      tc.setupAuth(t, request, server.tokenMaker)
+			server.router.ServeHTTP(recorder, request)
+			// check response
+			tc.checkResponse(t, recorder)
+		})
+	}
+}
+
+// TestCreateAccountAPI tests the createAccount API using mock database.
+func TestCreateAccountAPI(t *testing.T) {
+  user, _ := randomUser(t)
+	account := randomAccount(user.Username)
+  
+	testCases := []struct {
+		name          string
+    body          gin.H
+    setupAuth func(t *testing.T, request *http.Request, tokenMaker token.Maker)
+		buildStubs    func(store *mockdb.MockStore)
+		checkResponse func(t *testing.T, recorder *httptest.ResponseRecorder)
+	}{
+		// case 1: create account successfully.
+    {
+      name: "OK",
+      body: gin.H{
+        "currency": account.Currency,
+      },
+      setupAuth: func(t *testing.T, request *http.Request, tokenMaker token.Maker) {
+        addAuthorization(t, request, tokenMaker, authorizationTypeBearer, user.Username, time.Minute)
+      },
+      buildStubs: func(store *mockdb.MockStore) {
+        arg := db.CreateAccountParams{
+          Owner:      account.Owner,
+          Currency:   account.Currency,
+          Balance:    0,
+        }
+
+        store.EXPECT().
+          CreateAccount(gomock.Any(), gomock.Eq(arg)).
+          Times(1).
+          Return(account, nil)
+      },
+      checkResponse: func(t *testing.T, recorder *httptest.ResponseRecorder) {
+        require.Equal(t, http.StatusOK, recorder.Code)
+        requireBodyMatchAccount(t, recorder.Body, account)
+      },
+    },
+		// case 2: no authorization.
+    {
+      name: "NoAuthorization",
+      body: gin.H{
+        "currency": account.Currency,
+      },
+      setupAuth: func(t *testing.T, request *http.Request, tokenMaker token.Maker) {
+      },
+      buildStubs: func(store *mockdb.MockStore) {
+        store.EXPECT().
+          CreateAccount(gomock.Any(), gomock.Any()).
+          Times(0)
+      },
+      checkResponse: func(t *testing.T, recorder *httptest.ResponseRecorder) {
+        require.Equal(t, http.StatusUnauthorized, recorder.Code)
+      },
+    },
+		// case 3: internal sever error.
+    {
+      name: "InternalError",
+      body: gin.H{
+        "currency": account.Currency,
+      },
+      setupAuth: func(t *testing.T, request *http.Request, tokenMaker token.Maker) {
+        addAuthorization(t, request, tokenMaker, authorizationTypeBearer, user.Username, time.Minute)
+      },
+      buildStubs: func(store *mockdb.MockStore) {
+        store.EXPECT().
+          CreateAccount(gomock.Any(), gomock.Any()).
+          Times(1).
+          Return(db.Account{}, sql.ErrConnDone)
+      },
+      checkResponse: func(t *testing.T, recorder *httptest.ResponseRecorder) {
+        require.Equal(t, http.StatusInternalServerError, recorder.Code)
+      },
+    },
+		// case 4: using invalid currency.
+    {
+      name: "InvalidCurrency",
+      body: gin.H{
+        "currency": "invalid",
+      },
+      setupAuth: func(t *testing.T, request *http.Request, tokenMaker token.Maker) {
+        addAuthorization(t, request, tokenMaker, authorizationTypeBearer, user.Username, time.Minute)
+      },
+      buildStubs: func(store *mockdb.MockStore) {
+        store.EXPECT().
+          CreateAccount(gomock.Any(), gomock.Any()).
+          Times(0)
+      },
+      checkResponse: func(t *testing.T, recorder *httptest.ResponseRecorder) {
+        require.Equal(t, http.StatusBadRequest, recorder.Code)
+      },
+    },
+  }
+  
+	for i := range testCases {
+		tc := testCases[i]
+
+		t.Run(tc.name, func(t *testing.T) {
+			ctrl := gomock.NewController(t)
+			defer ctrl.Finish()
+
+			store := mockdb.NewMockStore(ctrl)
+
+			// build stubs
+			tc.buildStubs(store)
+
+			// start test server and send request
+			server := NewTestServer(t, store)
+			recorder := httptest.NewRecorder()
+
+      // Marshal body data to JSON
+      data, err := json.Marshal(tc.body)
+			require.NoError(t, err)
+
+      url := "/accounts"
+      request, err := http.NewRequest(http.MethodPost, url, bytes.NewReader(data))
+			require.NoError(t, err)
+      
       tc.setupAuth(t, request, server.tokenMaker)
 			server.router.ServeHTTP(recorder, request)
 			// check response
