bee6ac8e19db529fddf8c0c107e6917c40a9d907
bagashiz <work.bitsy@simplelogin.com>
2022-12-06 21:22:50 +0700

    implement gRPC API request validation

---
+++ gapi/error.go
@@ -0,0 +1,30 @@
+package gapi
+
+import (
+	"google.golang.org/genproto/googleapis/rpc/errdetails"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
+// fieldViolation creates a new field violation error.
+func fieldViolation(field string, err error) *errdetails.BadRequest_FieldViolation {
+	return &errdetails.BadRequest_FieldViolation{
+		Field:       field,
+		Description: err.Error(),
+	}
+}
+
+// invalidArgumentError creates a new invalid argument error based on the field violations.
+func invalidArgumentError(violations []*errdetails.BadRequest_FieldViolation) error {
+	badRequest := &errdetails.BadRequest{
+		FieldViolations: violations,
+	}
+	statusInvalid := status.New(codes.InvalidArgument, "invalid parameters")
+
+	statusDetails, err := statusInvalid.WithDetails(badRequest)
+	if err != nil {
+		return statusInvalid.Err()
+	}
+
+	return statusDetails.Err()
+}
